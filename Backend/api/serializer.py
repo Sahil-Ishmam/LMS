913ed89f6712e024920e526063810364468388ea
Sahil-Ishmam <sahiluddinishmam@gmail.com>
2025-07-08 11:38:06 +0600

    bug fixed and getting refresh and access token

--- Backend/api/serializer.py
+++ Backend/api/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from rest_framework_simplejwt import TokenObtainPairSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from userauths.models import Profile,User
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
