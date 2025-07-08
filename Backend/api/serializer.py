1ced1f9a2f8795e34b3fea11fad3a4d6e40666d8
Sahil-Ishmam <sahiluddinishmam@gmail.com>
2025-07-08 11:26:33 +0600

    tokenobtainpair serializer view and url are set

--- Backend/api/serializer.py
+++ Backend/api/serializer.py
@@ -1,5 +1,19 @@
 from rest_framework import serializers
+from rest_framework_simplejwt import TokenObtainPairSerializer
 from userauths.models import Profile,User
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        token['email'] = user.email
+        token['full_name'] = user.full_name
+        # ...
+
+        return token 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
